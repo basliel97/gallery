908daaf149e3850aababcd96ff7b7cbc6eeb6021
Basliel Getu <baslielgetu97@example.com>
2026-04-08 17:43:29 +0300

    last commit

--- middleware.ts
+++ middleware.ts
@@ -26,6 +26,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|login|signup|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+     "/((?!_next/static|_next/image|favicon.ico|login|signup|about|contact|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4)$).*)",
   ],
 };
